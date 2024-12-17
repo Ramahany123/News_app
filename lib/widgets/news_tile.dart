a7cb36bb89c31a3c36137ed4e2c7d0fcdabf6295
Rama Hany <hanyrama703@gmai.com>
2024-12-17 21:30:34 +0200

    added geasture detector to navigate to news web view

--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -1,50 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/articles_model.dart';
+import 'package:news_app/widgets/news_web_view.dart';
 
 class NewsTile extends StatelessWidget {
   const NewsTile({super.key, required this.article});
   final ArticlesModel article;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        ClipRRect(
-          //to give a border radius without the need for a container
-          borderRadius: BorderRadius.circular(8),
-          child: Image.network(
-            article.image ??
-                "https://ehelperteam.com/wp-content/uploads/2019/09/Broken-images.png",
-            width: double.infinity,
-            height: 200,
-            fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => NewsWebView(url: article.url),
           ),
-        ),
-        const SizedBox(
-          height: 16,
-        ),
-        Text(
-          article.title,
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          style: const TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.w600,
+        );
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClipRRect(
+            //to give a border radius without the need for a container
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              article.image ??
+                  "https://ehelperteam.com/wp-content/uploads/2019/09/Broken-images.png",
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        Text(
-          article.description,
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          style: const TextStyle(
-            fontSize: 16,
-            color: Color.fromARGB(255, 120, 119, 119),
+          const SizedBox(
+            height: 16,
           ),
-        ),
-      ],
+          Text(
+            article.title,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(
+            article.description,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              fontSize: 16,
+              color: Color.fromARGB(255, 120, 119, 119),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
