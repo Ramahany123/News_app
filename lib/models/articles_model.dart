9fdbbf235340af891550b73fce695fc3bb3bf50f
Rama Hany <hanyrama703@gmai.com>
2024-12-17 21:30:00 +0200

    added url attribute

--- lib/models/articles_model.dart
+++ lib/models/articles_model.dart
@@ -2,14 +2,19 @@ class ArticlesModel {
   final String title;
   final String description;
   final String? image;
+  final String url;
 
   ArticlesModel(
-      {required this.title, required this.description, required this.image});
+      {required this.title,
+      required this.description,
+      required this.url,
+      required this.image});
 
   factory ArticlesModel.fromJson(json) {
     return ArticlesModel(
         title: json["title"],
         description: json["description"],
+        url: json["url"],
         image: json["image"]);
   }
 }
